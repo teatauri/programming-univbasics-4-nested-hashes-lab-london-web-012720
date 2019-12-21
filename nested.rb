3b873ae65a0abdf676132b7554854183a663142f
Lee Bardon <leerbardon@gmail.com>
2019-12-21 21:04:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -132,7 +132,7 @@ def adding_to_dennis
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => "C"
+          :languages => ["C"]
         }
      }
      
