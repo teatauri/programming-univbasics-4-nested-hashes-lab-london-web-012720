0576a46a5c7721d57181299afe5f910c8de5a915
Lee Bardon <leerbardon@gmail.com>
2019-12-21 21:05:16 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+return programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -55,6 +59,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+programmer_hash[:dennis_ritchie][:languages].join()
 end
 
 def adding_matz
@@ -80,6 +86,12 @@ def adding_matz
         }
      }
 
+programmer_hash[:yukihiro_matsumoto] = {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+          }
+          
+return programmer_hash
     
 end
 
@@ -102,7 +114,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+programmer_hash[:alan_kay][:known_for] = "GUI"
+return programmer_hash
+
 end
 
 def adding_to_dennis
@@ -121,5 +135,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+
+return programmer_hash
 
 end
