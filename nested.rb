a66c0b9415ae61352e7eb99756bd89b715322771
Lee Bardon <leerbardon@gmail.com>
2019-12-21 20:53:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,10 +86,12 @@ def adding_matz
         }
      }
 
-programmer_hash[0] = :yukihiro_matsumoto => {
+programmer_hash[:yukihiro_matsumoto] => {
           :known_for => "Ruby",
           :languages => ["LISP", "C"]
-           }
+          }
+          
+return programmer_hash
     
 end
 
